6841e4b3b22907041ec3e247d90c437ba3b9f71f
Prashant <ppresh.ant@gmail.com>
2018-10-02 12:10:55 -0500

    1. Added block structure to code 2. Blocks divided for specific purposes (regular plots vs assay plots vs other custom code)

--- analysis.R
+++ analysis.R
@@ -1,41 +1,64 @@
 # Read in the file and do manual analysis and plotting
-#---
-# Source the general_functions file before running this
-# saved important/general analyses codes from console
+# Author: Prashant Kalvapalle;  October 10 2018
 
+# Source the general_functions file before running this
+# The last part of the script contains important codes from the console that were used for specific situations : These will be commented
+
+# plotting functions ----
+
+# plots all the Tm's if samples have multiple peaks in the melting curve
+plotalltms <- function(fl)
+{ 
+  # Gather the Tm's into another data frame and merge into 1 column
+  tmfl <- fl$Results %>% select(`Sample Name`, `Primer pair`, starts_with('Tm')) %>% gather('Peak number','Tm',-`Sample Name`, -`Primer pair`)
+  
+  # plot the Tm ; Graph will now show
+  plttm2 <- tmfl %>% ggplot(.) + aes(x = `Sample Name`, y = Tm) + geom_point(aes(color = `Peak number`), size = 2) +
+    theme_classic() + scale_color_brewer(palette="Set1") + 
+    theme(plot.title = element_text(hjust = 0.5),axis.text.x = element_text(angle = 90, hjust = 1, vjust = .3)) + 
+    ggtitle(paste(title_name,': Melting curves')) + facet_grid(~`Primer pair`)
+}
+
+# plot the first Tm only ; Graph will now show
+plottm1 <- function(fl)
+{ 
+  plttm <- fl$Results %>% ggplot(.) + aes(x = `Sample Name`, y = Tm1) + geom_point(color = 'red', size = 2) +
+    theme_classic() + scale_color_brewer(palette="Set1") + 
+    theme(plot.title = element_text(hjust = 0.5),axis.text.x = element_text(angle = 90, hjust = 1, vjust = .3)) + 
+    ggtitle(paste(title_name,': Melting curves')) + facet_grid(~`Primer pair`)
+}
+
+
+# Input the data + transformations ----
 # choose file name, starts in the same directory as Rproject
-flnm <- 'excel files/Chk2 MHT.xls'  
+flnm <- 'excel files/Tr12 MHT.xls'  
+title_name <-'Water troubleshooting: Tr12' 
 
 fl <- readqpcr(flnm) # read excel file exported by Quantstudio
 
-# Separate primer pair from sample name
-fl$Results <- separate(fl$Results,`Sample Name`,c('Sample Name','Primer pair'),' ')
+# Separate primer pair from sample name and make factors in the right order (same order as the plate setup)
+fl <- separate_primer_names(fl)
 
-# Factorise the sample name in the order for plotting: the avg and Stdev is already calculated by quantstudio
-fl$Results$`Sample Name` <- fl$Results$`Sample Name` %>% factor(levels = unique(.))
 
-# Factorise the primer pairs in the order for plotting
-fl$Results$`Primer pair` <- fl$Results$`Primer pair` %>% factor(levels = unique(.))
-
-# Gather the Tm's into another data frame and merge into 1 column
-tmfl <- fl$Results %>% select(`Sample Name`, `Primer pair`, starts_with('Tm')) %>% gather('Peak number','Tm',-`Sample Name`, -`Primer pair`)
+# Plots for typical troubleshooting or small scale assays (facetted by primer names)----
 
 # plot the Tm ; Graph will now show
-plttm <- fl$Results %>% ggplot(.) + aes(x = `Sample Name`, y = Tm1) + geom_point(color = 'red', size = 2) +
-  theme_classic() + scale_color_brewer(palette="Set1") + 
-  theme(plot.title = element_text(hjust = 0.5),axis.text.x = element_text(angle = 90, hjust = 1, vjust = .3)) + 
-  ggtitle('qPCR on Gibson: Chk2: Melting curves') + facet_grid(~`Primer pair`)
-
-# print(plttm)
+plttm2 <- plotalltms(fl) # plots tms of multiple peaks in melting curve
 
 # plot the CT mean along with replicates
 plt <- fl$Results %>% ggplot(.) + aes(x = `Sample Name`, y = CT) + geom_point(color = 'red', size = 1, show.legend = T) +
   geom_boxplot(aes(x = `Sample Name`, y = `Ct Mean`), show.legend = T) +
   theme_classic() + scale_color_brewer(palette="Set1") + 
   theme(plot.title = element_text(hjust = 0.5),axis.text.x = element_text(angle = 90, hjust = 1, vjust = .3)) + 
-  ggtitle('qPCR on Gibson: Chk2') + facet_grid(~`Primer pair`)
+  ggtitle(title_name) + facet_grid(~`Primer pair`)
 
 print(plt)
 
-# Save manually - copy paste this command
+# Save plots manually - copy paste this command to console ----
 # ggsave('qPCR analysis/Chk1.png', dpi = 600)
+# ggsave('qPCR analysis/Chk1.png', plot = plttm, dpi = 600)
+
+# Plots for Assays ----
+
+
+# Custom codes ----

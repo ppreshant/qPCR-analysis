fd87fc18123f5ebdc9c3fc4c7f3541579d63be29
ppreshant <ppresh.ant@gmail.com>
2019-10-09 17:51:10 -0500

    Adding plot formatting functions

--- general_functions.R
+++ general_functions.R
@@ -70,3 +70,41 @@ lm_eqn <- function(df, trig = 0){
     tsumrev <- trev %>% group_by(`Sample Name`) %>% summarise(CT = mean(CT), Quantity = mean(Quantity), CT_sd = sd(CT))
     diff(tsumrev$CT) %>% round(2)}
 
+  # plot formatting ---- 
+  
+  # plot formatting function : format as classic, colours = Set1
+  format_classic <- function(plt, title_name, plot_assay_variable)
+  { # formats plot as classic, with colour palette Set1, centred title, angled x axis labels
+    plt <- plt +
+      theme_classic() + scale_color_brewer(palette="Set1") + 
+      theme(plot.title = element_text(hjust = 0.5),axis.text.x = element_text(angle = 90, hjust = 1, vjust = .3)) +
+      ggtitle(title_name) + xlab(plot_assay_variable)
+  }
+  
+  # plot formatting function : format as logscale
+  format_logscale <- function(plt)
+  { # extra comments
+    plt <- plt +
+      scale_y_log10(  # logscale for y axis with tick marks
+        labels = fancy_scientific
+        #labels = scales::trans_format("log10", scales::math_format(10^.x) )
+      )
+  }
+  
+  # formatting labels in logscale cleanly : a x 10^b
+  fancy_scientific <- function(l) {
+    # turn in to character string in scientific notation
+    l <- format(l, scientific = TRUE)
+    # quote the part before the exponent to keep all the digits
+    l <- gsub("^(.*)e", "'\\1'e", l)
+    # remove + after exponent, if exists. E.g.: (3x10^+2 -> 3x10^2) 
+    l <- gsub("e\\+","e",l)
+    # turn the 'e+' into plotmath format
+    l <- gsub("e", "%*%10^", l)
+    # convert 1x10^ or 1.000x10^ -> 10^ 
+    l <- gsub("\\'1[\\.0]*\\'\\%\\*\\%", "", l)
+    # return this as an expression
+    parse(text=l)
+  }
+  
+  # use as ggplot(df,aes(x,y)) + geom_point() + scale_y_log10(labels = fancy_scientific)

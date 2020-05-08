a5a8e7b0eb03e59fb457b432728b5b803311fa36
ppreshant <ppresh.ant@gmail.com>
2020-05-07 23:48:38 -0500

    Trying to analyze pr3 hybrid MHT data with sybr and Fam

--- raw_data_visualized.R
+++ raw_data_visualized.R
@@ -6,7 +6,7 @@
 library(readxl); library(magrittr); library(tidyverse); library(ggrepel); library(plotly)
 
 # Enter file name
-flnm <- 'q-S023b GFP 18-2-20'  # set the filename
+flnm <- 'Pr4 hybrid MHT'  # set the filename : q-S023b GFP 18-2-20
 flpath <- str_c('excel files/',flnm,'.xls') # this completes the file path
 
 # load file and gather specific data range from specific sheet
@@ -16,11 +16,11 @@ multidat <- read_excel(flpath, sheet = 'Multicomponent Data', range = 'A36:E1578
 # Convert data to long form
 combdat <- list(raw = rawdat, mult = multidat) %>% map_dfr( ~gather(., key = 'channel', 'signal', -Well, -'Well Position', -Cycle))
 
-# filtering a single well data = A1
+# filtering a single well data = A2
 
 # plotting normal
-plt1 <- combdat %>% filter(`Well Position` == 'A1') %>%  ggplot(aes(Cycle, signal, colour = channel)) + geom_point() + geom_line()
+plt1 <- combdat %>% filter(`Well Position` == 'A2') %>%  ggplot(aes(Cycle, signal, colour = channel)) + geom_point() + geom_line()
 
 # interactive plot
-combdat %>% filter(`Well Position` == 'A1') %>%  plot_ly(x = ~Cycle, y = ~signal, color = ~channel, type = 'scatter', mode = 'lines+markers')
+combdat %>% filter(`Well Position` == 'A2') %>%  plot_ly(x = ~Cycle, y = ~signal, color = ~channel, type = 'scatter', mode = 'lines+markers')
 

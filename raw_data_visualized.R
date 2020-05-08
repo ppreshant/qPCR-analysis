11aeee487bd93bb7218d6e9f7649eb185afb8a76
ppreshant <ppresh.ant@gmail.com>
2020-05-07 23:37:09 -0500

    Script to bring out raw data (4 channels) and multicomponent data sheets from any qPCR output file - Draws intereractive plotly plot as well

---
+++ raw_data_visualized.R
@@ -0,0 +1,26 @@
+# Script to analyze raw data and multicomponent data of qPCR files by hand
+# Author : Prashant
+# data = 7 May 2020
+
+# calling libraries ; make sure they are installed (install.packages)
+library(readxl); library(magrittr); library(tidyverse); library(ggrepel); library(plotly)
+
+# Enter file name
+flnm <- 'q-S023b GFP 18-2-20'  # set the filename
+flpath <- str_c('excel files/',flnm,'.xls') # this completes the file path
+
+# load file and gather specific data range from specific sheet
+rawdat <- read_excel(flpath, sheet = 'Raw Data', range = 'A36:G15780')
+multidat <- read_excel(flpath, sheet = 'Multicomponent Data', range = 'A36:E15780')
+
+# Convert data to long form
+combdat <- list(raw = rawdat, mult = multidat) %>% map_dfr( ~gather(., key = 'channel', 'signal', -Well, -'Well Position', -Cycle))
+
+# filtering a single well data = A1
+
+# plotting normal
+plt1 <- combdat %>% filter(`Well Position` == 'A1') %>%  ggplot(aes(Cycle, signal, colour = channel)) + geom_point() + geom_line()
+
+# interactive plot
+combdat %>% filter(`Well Position` == 'A1') %>%  plot_ly(x = ~Cycle, y = ~signal, color = ~channel, type = 'scatter', mode = 'lines+markers')
+

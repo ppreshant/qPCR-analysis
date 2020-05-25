e55775180b8a1e89385ca82b099486ec87604435
ppreshant <ppresh.ant@gmail.com>
2020-05-25 01:36:27 -0500

    Changed to S06d, added comments - was Pr4 hybrid MHT initially

--- raw_data_visualized.R
+++ raw_data_visualized.R
@@ -6,15 +6,15 @@
 library(readxl); library(magrittr); library(tidyverse); library(ggrepel); library(plotly)
 
 # Enter file name
-flnm <- 'Pr4 hybrid MHT'  # set the filename : q-S023b GFP 18-2-20
+flnm <- 'S06d'  # set the filename : q-S023b GFP 18-2-20
 flpath <- str_c('excel files/',flnm,'.xls') # this completes the file path
 
 # load file and gather specific data range from specific sheet
-rawdat <- read_excel(flpath, sheet = 'Raw Data', range = 'A36:G15780')
-multidat <- read_excel(flpath, sheet = 'Multicomponent Data', range = 'A36:E15780')
+rawdat <- read_excel(flpath, sheet = 'Raw Data', range = 'A36:G15780') # reading raw data from 4 channels
+multidat <- read_excel(flpath, sheet = 'Multicomponent Data', range = 'A36:E15780') # reading extrapolated data for each fluorophore
 
 # Convert data to long form
-combdat <- list(raw = rawdat, mult = multidat) %>% map_dfr( ~gather(., key = 'channel', 'signal', -Well, -'Well Position', -Cycle))
+combdat <- list(raw = rawdat, mult = multidat) %>% map_dfr( ~gather(., key = 'channel', 'signal', -Well, -'Well Position', -Cycle)) # combine all the channels and fluorophores into a pair of name - value columns (long data)
 
 # filtering a single well data = A2
 

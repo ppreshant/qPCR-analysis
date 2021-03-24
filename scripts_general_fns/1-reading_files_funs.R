2432faa73a92afa9907aa46a2b910fbd043fef1d
ppreshant <ppresh.ant@gmail.com>
2021-03-24 01:30:04 -0500

    1-reading files : Reads q0xx number in the plate name to get template, Sample_name changed to Sample_name_bulk for clarity - Skipping 41 lines instead of 38 -- to account for calibration expiry

--- scripts_general_fns/1-reading_files_funs.R
+++ scripts_general_fns/1-reading_files_funs.R
@@ -7,7 +7,9 @@ readqpcr <- function(flnm)
   fl <- flnm %>%  
     excel_sheets() %>% 
     set_names(.,.) %>% 
-    map(read_excel, path = flnm, skip = 38)
+    map(read_excel, path = flnm, skip = 41) # change skip # if channels are not calibrated
+  
+  # put an error here if column names are not read properly (maybe look for well position and one other column..)
   
   # convert CT values into numeric 
   class(fl$Results$CT) <- 'numeric'
@@ -16,14 +18,15 @@ readqpcr <- function(flnm)
 
 
 # Gets the 96 well layout with template names matching the experiment ID from filename in a google sheet
-get_template_for <- function(bait, sheet_url = sheeturls$templates)
+get_template_for <- function(bait, sheet_url = sheeturls$plate_layouts_PK)
 { # Looking for WWx or Stdx - example WW21 or Std7 within the filename; Assumes plate spans from row B to N (1 row below the matching ID)
   
   # Finding the plate to be read
   plate_names_row <- read_sheet(sheet_url, sheet = 'qPCR plate layouts', range = 'C:C', col_types = 'c')
   m_row <- plate_names_row %>% unlist() %>% as.character() %>% 
-    # find the row with standard beginings matching the filename
-    str_detect(., bait) %>% 
+    # find the row with standard beginnings matching the filename
+    str_detect(., str_c('^', bait %>% str_match('^(q)[:digit:]*') %>% .[1]) ) %>% 
+    # str_detect(., bait) %>% 
     which() + 1
   range_to_get <- str_c('B', m_row + 1, ':N', m_row + 9)
   
@@ -34,6 +37,6 @@ get_template_for <- function(bait, sheet_url = sheeturls$templates)
   plate_template_raw <- read_sheet(sheet_url, sheet = 'qPCR plate layouts', range = range_to_get)
   
   # Convert the 96 well into a single column, alongside the Well position
-  plate_template <- read_plate_to_column(plate_template_raw, 'Sample_name') # convert plate template (Sample_names) into a single vector, columnwise
+  plate_template <- read_plate_to_column(plate_template_raw, 'Sample_name_bulk') # convert plate template (Sample_names) into a single vector, columnwise
   
 }

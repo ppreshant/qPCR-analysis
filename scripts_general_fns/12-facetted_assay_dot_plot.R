05eaf271669d263ab24412cc4f8eb8bc1eaf6c1f
ppreshant <ppresh.ant@gmail.com>
2021-04-03 01:13:06 -0500

    12-faceted assay dot plot : creating a generalized plot, with flexible x, y, colour and single facet. Default values make it easy to plot similar plot with different Y axis

---
+++ scripts_general_fns/12-facetted_assay_dot_plot.R
@@ -0,0 +1,28 @@
+# g.12 facetted assay dot plot
+# Prashant, 2/April/21
+
+# Will start with certain specific features. 
+# More hard-coded variabels can be user-input as use cases arrive
+
+plot_facetted_assay <- function(.data = polished_cq.dat,  # data.frame or tibble
+                                # required variable .yvar_plot
+                                .yvar_plot, .xvar_plot = assay_variable, # variables within the dataframe (not string, use raw name)
+                                .colourvar_plot = Sample_name,
+                                .facetvar_plot = Target_name, # facets align along the x axis, cannot be NULL as of now
+                                .subtitle.plot = NULL) # 'y axis label' supplied as a string to .subtitle.plot
+{
+  
+  # check missing variables
+  if(is.null(enquo(.yvar_plot))) stop('Missing y axis variable for plot in plot_facetted_assay call')  
+  
+  # Get plot subtitle
+  if(is.null(.subtitle.plot)) .subtitle.plot <- yaxis_translation[deparse(enexpr(.yvar_plot))]
+  
+  # plotting
+  {ggplot(.data, aes(x = {{.xvar_plot}}, y = {{.yvar_plot}}, colour = {{.colourvar_plot}})) +
+      geom_point() +
+      facet_grid(cols = vars({{.facetvar_plot}}), scales = 'free_x', space = 'free_x') + 
+      ggtitle(title_name, subtitle = .subtitle.plot) + 
+      {if(!is.null(.subtitle.plot)) ylab('')} }  %>%  # remove y axis label if a subtitle is provided which is more readable
+    print()
+}

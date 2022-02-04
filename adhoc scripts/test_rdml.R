2dbf6dbffd321a2ad46379ca28e0120141fad0d1
ppreshant <ppresh.ant@gmail.com>
2022-02-04 04:23:41 -0600

    test_rdml.R : test rdml file read-write-read. Still unable to be read by rdmlpython and rdml-ninja

--- adhoc scripts/test_rdml.R
+++ adhoc scripts/test_rdml.R
@@ -1,22 +1,39 @@
 # playing with rdml files
 # Prashant K ; 17/jan/2022
 
-# load modules
+# load modules ----
 library(RDML)
 source('./0-general_functions_main.R') # Source the general_functions file before running this
 
 
 
-# User inputs
-file_rdml <- 'S019 25-11-19.rdml'
+# User inputs ----
+file_rdml <- 'S019_25-11-19_v152_dyefixed'
 
-dirpath <- 'excel files/'
+dirpath <- 'RDML_files/'
 
-flpath <- str_c(dirpath, file_rdml)
+flpath <- str_c(dirpath, file_rdml, '.rdml')
 
-# Load file
+# Load file ----
 fl <- RDML$new(flpath)
+
+# get fluorescence data (amplification step)
 fl.data <- fl$GetFData()
 
-fl$target
 
+# validation ----
+
+# check if dye is recognized : fixed v152 files
+fl$dye$SYBR$id
+
+# check that dyeID is attached to one target
+fl$target$flipped$dyeId
+
+
+# write RDML ----
+fl$AsXML(str_c(dirpath, file_rdml, "_AsXML.rdml")) # This file can't be read by rdmlpython, why?
+
+# try reading back in : WORKS
+fl2 <- RDML$new(str_c(dirpath, file_rdml, "_AsXML.rdml"))
+
+fl2$target$flipped

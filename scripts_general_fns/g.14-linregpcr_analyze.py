9c29e4880fa4c9246bcec67c8c17d3147454c672
ppreshant <ppresh.ant@gmail.com>
2022-02-16 01:22:58 -0600

    rewriting linregpcr analysis as a python function; need to invoke from R/reticulate workflow. Also started a python general utils for commandline ease like pipes

--- scripts_general_fns/g.14-linregpcr_analyze.py
+++ scripts_general_fns/g.14-linregpcr_analyze.py
@@ -9,44 +9,65 @@ Created on Mon Jan 17 15:20:18 2022
 inspiration # from : http://rdml.org/referenceImplement.html
 """
 
-import rdmlpython as rdml
+def linregpcr_analyze(filename_rdml = 'S019_25-11-19_v152'):
 
-# User input
-filename_rdml = 'S019_25-11-19_v152'
-dirname = 'RDML_files/'
-
-flpath = dirname + filename_rdml + '.rdml'
-
-# Read file
-fl = rdml.Rdml(flpath)
-
-
-# Fix the dyes issue for validation purposes
-# hint : fl.dyes()[1].tojson()['id']
-
-# get all dues used in the RDML file
-dyeIds = set() # initialize empty set. Prevents duplicates so better than list
-for targetfield in fl.targets():
-    dyeIds.add(targetfield.tojson()['dyeId'])
-
-# add all these dyes into the rdml file
-for dye in dyeIds:
-    fl.new_dye(dye)
-
-# Validate a file
-print(fl.validate())
-# doesn't validate here but works in RDML-ninja
-
-# Run LinRegPCR
-
-# unpack the rdml file, 1st experiment, 1st run
-qpcr_result = fl.experiments()[0].runs()[0].linRegPCR(updateRDML=True, saveResultsCSV=True, timeRun=True, verbose=True)
+    import rdmlpython as rdml # load package
     
-
-# save results to RDML
-fl.save(dirname + "results/" + filename_rdml + "fixpython.rdml")
-
-# save results as csv file
-with open('excel files/' + filename_rdml + ".csv", "w") as cli_f:
-    cli_f.write(qpcr_result["resultsCSV"])
+    # User input
+    #filename_rdml = 'S019_25-11-19_v152'
+    # directory -- relative might be an issue..
+    dirname = 'RDML_files/'
+    
+    flpath = dirname + filename_rdml + '.rdml'
+    
+    # Read file
+    fl = rdml.Rdml(flpath)
+    
+    
+    # Fix the dyes issue for validation purposes
+    
+    # get all dyes used in the RDML file
+    dyeIds = set() # initialize empty set. Prevents duplicates so better than list
+    for targetfield in fl.targets():
+        dyeIds.add(targetfield.tojson()['dyeId'])
+    
+    # add all these dyes into the rdml file
+    for dye in dyeIds:
+        fl.new_dye(dye)
+    
+    # Validate a file
+    # print(fl.validate())
+    # doesn't validate here but works in RDML-ninja
+    
+    # Run LinRegPCR
+    
+    # unpack the rdml file, 1st experiment, 1st run
+    first_run_data = fl.experiments()[0].runs()[0]
+    
+    # amplification data - run linregpcr
+    qpcr_result = first_run_data.\
+        linRegPCR(updateRDML=True,
+                  saveResultsCSV=True, 
+                  timeRun=True, 
+                  verbose=True)
         
+    # run melt curve only if dye chemistry (FAM or SUN absent in dyelist)
+    if('FAM' or 'SUN' not in dyeIds):      
+        melt_curve_result = first_run_data.\
+            meltCurveAnalysis(updateRDML=True,
+                              saveResultsCSV=True,
+                              verbose=True)
+     # melt curve not working with default parameters -- same as online   
+    
+    # save results to RDML
+    fl.save(dirname + "results/" + filename_rdml + "fixpython.rdml")
+    
+    # save amplification analysis results as csv file
+    with open('excel files/' + filename_rdml + ".csv", "w") as filehandle:
+        filehandle.write(qpcr_result["resultsCSV"])
+            
+    # save melt curve analysis results as csv file: if a non empty result exists
+    if('melt_curve_result' in locals() and len(melt_curve_result)):  
+        with open('excel files/' + filename_rdml + "-melt.csv", "w") as filehandle:
+            filehandle.write(melt_curve_result["resultsCSV"])
+
---
+++ scripts_general_fns/g.15-python_utils.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 15 16:41:15 2022
+
+@author: Prashant
+Purpose: Packages for python loading, commandline usage
+"""
+
+# pipes using sspipes package : simple smart pipes
+# https://pypi.org/project/sspipe/
+# usage: x | p(fn) | p(fn2, arg1, arg2 = px)
+#--------------------------------
+from sspipe import p, px
+

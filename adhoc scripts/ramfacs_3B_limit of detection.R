1f74f6506611aaf21e0e8b1fc56bfeae5ebf997d
ppreshant <ppresh.ant@gmail.com>
2022-06-20 13:55:10 -0500

    3B_limit of detection : fixed bug with plt_copies being reassigned - Added geom_smooth for a subset of data

--- adhoc scripts/ramfacs_3B_limit of detection.R
+++ adhoc scripts/ramfacs_3B_limit of detection.R
@@ -14,7 +14,7 @@ title_name <-'3B Limit of detection of splicing-qPCR'
 sample_name_translator <- c('Base strain' = 'Inf', # changes the LHS into the RHS
                             'ntc' = 'Inf',
                             '51' = '0.5',
-                            '1/|,' = '') 
+                            '1/|,' = '') # remove commas and convert the 1/x into x 
 
 # Input the data ----
 
@@ -39,14 +39,27 @@ plt_copies <- {plot_facetted_assay(.yvar_plot = Copies.per.ul.template, .xvar_pl
   format_logscale_x() %>% format_logscale_y()
 
 
-plt_cq <- plt_copies <- {plot_facetted_assay(.yvar_plot = 40 - CT, .xvar_plot = `Plasmid containing cell fraction`,
+plt_cq <- {plot_facetted_assay(.yvar_plot = 40 - CT, .xvar_plot = `Plasmid containing cell fraction`,
                                              .xaxis.label.custom = NULL) +
     theme(legend.position = 'top')} %>% 
   format_logscale_x()
 
-# interactive
+# Single panel plot
+plt_monopanel_copies_w_mean <- {plot_facetted_assay(.yvar_plot = Copies.per.ul.template, .xvar_plot = `Plasmid containing cell fraction`,
+                                             .facetvar_plot = NULL, .colourvar_plot = Target_name,
+                                   .xaxis.label.custom = NULL) +
+    
+    # geom_point(aes(y = mean_Copies.per.ul.template), shape = '-', size = 5, show.legend = FALSE) + # show the means
+    geom_smooth(aes(y = mean_Copies.per.ul.template), 
+                data = filter(forplotting_cq.dat, `Plasmid containing cell fraction` > 1e-5), # constrain data
+                method = 'lm', 
+                alpha = .01, show.legend = FALSE) +
+    
+    theme(legend.position = 'top')} %>% 
+  format_logscale_x() %>% format_logscale_y()
 
 
-# Save plot
+# Save plot ----
 
 ggsave(plot_as(title_name), plt_copies, width = 4.5, height = 4)
+ggsave(plot_as('q32_copies', '-w line'), plt_monopanel_copies_w_mean, width = 3.5, height = 4)

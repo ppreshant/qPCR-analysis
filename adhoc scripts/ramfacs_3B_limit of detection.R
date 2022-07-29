958da2e85c915fe1c713d1593e5b5d31e5fe3f4a
ppreshant <ppresh.ant@gmail.com>
2022-07-29 07:17:55 -0500

    ramfacs_3B_limit of detection : changed target names to be more meaningful to general audience

--- adhoc scripts/ramfacs_3B_limit of detection.R
+++ adhoc scripts/ramfacs_3B_limit of detection.R
@@ -16,6 +16,10 @@ sample_name_translator <- c('Base strain' = 'Inf', # changes the LHS into the RH
                             '51' = '0',
                             '1/|,' = '') # remove commas and convert the 1/x into x 
 
+target_name_translator <- c('U64' = 'Spliced product',
+                             'gfpbarcode' = 'Ribozyme',
+                             '16s' = '16S rRNA') # regex to change the target names for publication
+
 # Input the data ----
 
 .df <- get_processed_datasets(flnm)
@@ -28,7 +32,9 @@ forplotting_cq.dat <- filter(.df, !str_detect(Sample_category, 'conjug')) %>%
            (str_replace_all(assay_variable, sample_name_translator) %>% 
               as.numeric %>% {1/(1+.)} # convert to numbers and to fraction (1:1 -> 0.5, 1:10 -> 1/11 ..)
             ),
-         .before = assay_var.horz_label)
+         .before = assay_var.horz_label) %>% 
+  
+  mutate(across(Target_name, ~ str_replace_all(.x, target_name_translator)))
 
 
 # Plotting ----
@@ -59,11 +65,30 @@ plt_monopanel_copies_w_mean <- {plot_facetted_assay(.yvar_plot = Copies.per.ul.t
   format_logscale_x() %>% format_logscale_y()
 
 
+# plot only U64 for presentation
+
+plt_U64_copies_w_mean <- 
+  {plot_facetted_assay(.data = filter(forplotting_cq.dat, str_detect(Target_name, 'Spliced')),
+                       
+                       .yvar_plot = Copies.per.ul.template, .xvar_plot = `fraction of RAM cells`,
+                       .facetvar_plot = NULL, .colourvar_plot = NULL,
+                       .xaxis.label.custom = NULL) +
+      
+    # geom_point(aes(y = mean_Copies.per.ul.template), shape = '-', size = 5, show.legend = FALSE) + # show the means
+    geom_smooth(aes(y = mean_Copies.per.ul.template), 
+                data = filter(forplotting_cq.dat, str_detect(Target_name, 'Spliced'), `fraction of RAM cells` > 1e-5), # constrain data
+                method = 'lm', 
+                alpha = .01, show.legend = FALSE) +
+    
+    theme(legend.position = 'top')} %>% 
+  format_logscale_x() %>% format_logscale_y()
+
 # Save plot ----
 
 ggsave(plot_as('q32_Cq'), plt_copies, width = 4.5, height = 4)
 ggsave(plot_as(title_name), plt_copies, width = 4.5, height = 4)
-ggsave(plot_as('q32_copies', '-w line'), plt_monopanel_copies_w_mean, width = 3.5, height = 4)
+ggsave(plot_as('q32_copies', '-w line'), plt_monopanel_copies_w_mean, width = 3.7, height = 4)
+ggsave(plot_as('q32_copies-U64', '-w line'), plt_U64_copies_w_mean, width = 3.7, height = 4)
 
 
 # Save data ----

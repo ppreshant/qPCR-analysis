622ae5c18071479d8be2a1bd29e25b1b9008d11d
ppreshant <ppresh.ant@gmail.com>
2023-04-01 23:48:02 -0500

    Added timeseries plots - facets by organism

--- adhoc scripts/S067_q45_plots.R
+++ adhoc scripts/S067_q45_plots.R
@@ -3,7 +3,7 @@
 # Prelims -----
 source('./0-general_functions_main.R') # Source the general_functions file before running this
 source('./0.5-user-inputs.R') # source the user inputs from a different script
-
+title_name <- base_title_name
 
 # Load data ----
 
@@ -13,14 +13,27 @@ forplotting_cq.dat <- get_processed_datasets(flnm) %>%
   
   # split columns
   separate(assay_variable, into = c('plasmid', 'organism'), sep = '/', remove = FALSE) %>% 
-  separate(Sample_category, into = c('day', 'induction'), sep = '/', remove = FALSE) %>% 
+  separate(Sample_category, into = c('day', 'AHL (uM)'), sep = '/', remove = FALSE) %>% 
   
   # change types
-  mutate(across(induction, 
+  mutate(across(`AHL (uM)`, 
                 ~ if_else(str_detect(., 'I'), 10, 0) %>% # Induced = 10, uninduced
-                  replace_na(0)))  # NA is 0
+                  replace_na(0) %>% as.character %>% fct_inorder),   # NA is 0
+         across(organism, ~ replace_na(., 'control')),
+         across(day, ~ str_replace_all(., c('control' = 'd-1', '^d' = ''))))
 
 # TODO : take ratio of flipped / backbone ; plot ratio in a timecourse ; 
+
+
+# Ratios ----
+
+# take ratio to backbone
+ratio_data <- select(forplotting_cq.dat, -CT) %>% # remove the non unique columns
+  pivot_wider(names_from = Target_name, values_from = Copies_proportional) %>% 
+  
+  mutate(plasmid_copy_number = backbone/chromosome,
+         flipped_fraction = flipped/backbone)
+
 
 
 # Plotting ----
@@ -30,65 +43,30 @@ forplotting_cq.dat <- get_processed_datasets(flnm) %>%
 
 
 # Small multiples-timeseries plot
-processed.data %<>% mutate(across(Samples, ~ fct_relevel(., rev(sample_order)))) # order in desc of max
-
-title_name <- 'S067b wastewater memory II'
-
-filter(processed.data, category == 'Memory') %>% 
-  
-  ggplot(aes(Day, `RFP/OD`, colour = category, shape = `AHL (uM)`)) + 
-  
-  geom_point(size = 2) +
-  # scale_colour_brewer(palette = 'Dark2', direction = -1) + # change the values - orange for uninduced/0
-  scale_shape_manual(values = c(1, 16)) + # shape : open and closed circles
-  
-  # line like a dumbell plot
-  geom_line(aes(group = interaction(`AHL (uM)`, category)), colour = 'black', alpha = 0.3) +  
-  
-  facet_wrap(vars(Samples), scale = 'free_y') +
-  theme(legend.position = 'top') +
-  
-  # labels
-  ggtitle('Memory in wastewater', subtitle = title_name)
-
-ggsave(plot_as(title_name, '-timeseries-memory'), width = 5, height = 5)
-
-
-# overview plot
-ggplot(S67b2_order,
-       aes(`RFP/OD`, Samples, colour = Day, shape = `AHL (uM)`)) + 
-  
-  geom_point(size = 2) +
-  # scale_colour_brewer(palette = 'Dark2', direction = -1) + # change the values - orange for uninduced/0
-  scale_shape_manual(values = c(1, 16)) + # shape : open and closed circles
-  
-  # line like a dumbell plot
-  geom_line(aes(group = Samples), colour = 'black', alpha = 0.3) + 
-  
-  # facet_grid(vars(category), scale = 'free_y', space = 'free_y') +
-  theme(legend.position = 'top') +
-  
-  # labels
-  ggtitle('Memory in wastewater', subtitle = title_name)
-
-ggsave(plot_as(title_name), width = 4, height = 6)
-
-
-
-# Doesn't work
-horz.cq <- {plot_facetted_assay(.yvar_plot = 40-CT, .xvar_plot = assay_var.horz_label, 
-                                .xaxis.label.custom = axislabel.assay_variable) + 
-    guides(colour = guide_legend(title = '', nrow = 2))} %>%  # remove legend title
+timeseries <- 
+  {filter(ratio_data, organism != 'control', plasmid != '143') %>% # remove empty data
+      # filter(forplotting_cq.dat, Target_name == 'backbone') %>% 
+      
+      ggplot(aes(day, flipped_fraction, colour = plasmid, shape = `AHL (uM)`)) + 
+      
+      geom_point(size = 2) +
+      # scale_colour_brewer(palette = 'Dark2', direction = -1) + # change the values - orange for uninduced/0
+      scale_shape_manual(values = c(1, 16)) + # shape : open and closed circles
+      scale_alpha_discrete(guide = 'none', range = c(0.5, 1)) + # control line transparency
+      
+      
+      # line like a dumbell plot
+      geom_line(aes(group = interaction(plasmid, `AHL (uM)`, biological_replicates), 
+                    alpha = `AHL (uM)`)) +  
+      
+      facet_wrap(vars(organism), scale = 'free_y') +
+      theme(legend.position = 'top') +
+      
+      # labels
+      ggtitle('Memory in wastewater', subtitle = title_name)} %>% 
   
   print()
 
-
-horz.cq + ggrepel::geom_text_repel(aes(label = biological_replicates), show.legend = F,
-                                   data = filter(forplotting_cq.dat, assay_variable == 'water'))
-ggsave(plot_as(title_name, '-overview'))
-
-# interactive  
-plotly::ggplotly(horz.cq)
-
-
+ggplotly(timeseries)
+ggsave(plot_as(title_name, '-timeseries'), width = 5, height = 5)
 

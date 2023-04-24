cb90456b97dc4e67941d75d3f3b95ee3e5685ba7
ppreshant <ppresh.ant@gmail.com>
2023-04-24 13:19:09 -0500

    Modified for S071_q48 plots from S067_q45 (reduce redundancy, ~ loss of generality)

--- adhoc scripts/S071_q48_memory_timeseries.R
+++ adhoc scripts/S071_q48_memory_timeseries.R
@@ -1,23 +1,41 @@
-# S067_q45_plots.R
+# S071_q48_memory_timeseries.R
+
+# copied elements from S050_q37_41 ; subsumed: S067_q45_plots.R --> change inputs to do this
+
 
 # Prelims -----
 source('./0-general_functions_main.R') # Source the general_functions file before running this
 source('./0.5-user-inputs.R') # source the user inputs from a different script
 
 # override filename and title name from user inputs 
-flnm <- 'q45_S067_top3 till d2_29-3-23'
-title_name <- 'q45_S067_top3'
+title_name <- 'S071_q48_pilot 1'
+flnms <- c('q48a_S071_22-4-23', 'q48b_S071_23-4-23')
+
+
+# For S067 use these instead of above
+# flnm <- 'q45_S067_top3 till d2_29-3-23'
+# title_name <- 'q45_S067_top3'
+
+
+# translate plasmid names (assay_variable) into meaningful names
+plasmid_translation <- c('pPK' = '',
+                         '143' = 'No memory',
+                         '144|148' = 'Silent',
+                         '147|150' = 'Frugal',
+                         'pSS079|79' = 'Fluorescent')
+
 
 # Load data ----
 
-forplotting_cq.dat <- get_processed_datasets(flnm) %>% 
-
-  clean_up_water_wells() %>% 
+processed_data <- get_processed_datasets(flnms) %>% # clean_up_water_wells() %>% # for q45_S067
   
   # split columns
   separate(assay_variable, into = c('plasmid', 'organism'), sep = '/', remove = FALSE) %>% 
   separate(Sample_category, into = c('day', 'AHL (uM)'), sep = '/', remove = FALSE) %>% 
   
+  # translate assay_variable for presentation plot
+  mutate(across(plasmid, ~ str_replace_all(.x, plasmid_translation))) %>% 
+
   # change types
   mutate(across(`AHL (uM)`, 
                 ~ if_else(str_detect(., 'I'), 10, 0) %>% # Induced = 10, uninduced
@@ -28,23 +46,18 @@ forplotting_cq.dat <- get_processed_datasets(flnm) %>%
 # Ratios ----
 
 # take ratio to backbone
-ratio_data <- select(forplotting_cq.dat, -CT) %>% # remove the non unique columns
+ratio_data <- select(processed_data, -CT) %>% # remove the non unique columns
   pivot_wider(names_from = Target_name, values_from = Copies_proportional) %>% 
   
   mutate(plasmid_copy_number = backbone/chromosome,
          flipped_fraction = flipped/backbone)
 
+# Summary plot ----
 
+# Small multiples-timeseries plot : Modified from plate reader
 
-# Plotting ----
-
-
-# timeseries plot from plate reader
-
-
-# Small multiples-timeseries plot
 timeseries <- 
-  {filter(ratio_data, organism != 'control', plasmid != '143') %>% # remove empty data
+  {filter(ratio_data, organism != 'control', plasmid != 'No memory') %>% # remove empty data
       # filter(forplotting_cq.dat, Target_name == 'backbone') %>% 
       
       ggplot(aes(day, flipped_fraction, colour = plasmid, shape = `AHL (uM)`)) + 
@@ -60,7 +73,10 @@ timeseries <-
                     alpha = `AHL (uM)`)) +  
       
       facet_wrap(vars(organism), scale = 'free_y') +
+      
+      # legend
       theme(legend.position = 'top') +
+      guides(shape = guide_legend(nrow = 2)) +
       
       # labels
       ggtitle('Memory in wastewater', subtitle = title_name)} %>% 
@@ -68,33 +84,33 @@ timeseries <-
   print()
 
 ggplotly(timeseries)
-ggsave(plot_as(title_name, '-timeseries'), width = 5, height = 5)
+ggsave(plot_as(title_name, '-fraction'), width = 5, height = 5)
 
 
-# individual targets ----
+# individual target plot ----
 
 plot_timeseries_target <- function(filter_target = 'flipped')
-  {
-    filter(forplotting_cq.dat, organism != 'control', plasmid != '143', # remove empty data
-           Target_name == filter_target) %>% # filter specific target
-      
-      ggplot(aes(day, Copies_proportional, colour = plasmid, shape = `AHL (uM)`)) + 
-      
-      geom_point(size = 2) +
-      # scale_colour_brewer(palette = 'Dark2', direction = -1) + # change the values - orange for uninduced/0
-      scale_shape_manual(values = c(1, 16)) + # shape : open and closed circles
-      scale_alpha_discrete(guide = 'none', range = c(0.5, 1)) + # control line transparency
-      
-      # line connect data
-      geom_line(aes(group = interaction(plasmid, `AHL (uM)`), 
-                    alpha = `AHL (uM)`)) +  
-      
-      facet_wrap(vars(organism), scale = 'free_y') +
-      theme(legend.position = 'top') +
-      
-      # labels
-      ggtitle(str_c('Memory in wastewater : ', filter_target), subtitle = title_name)
-  }
+{
+  filter(forplotting_cq.dat, organism != 'control', plasmid != '143', # remove empty data
+         Target_name == filter_target) %>% # filter specific target
+    
+    ggplot(aes(day, Copies_proportional, colour = plasmid, shape = `AHL (uM)`)) + 
+    
+    geom_point(size = 2) +
+    # scale_colour_brewer(palette = 'Dark2', direction = -1) + # change the values - orange for uninduced/0
+    scale_shape_manual(values = c(1, 16)) + # shape : open and closed circles
+    scale_alpha_discrete(guide = 'none', range = c(0.5, 1)) + # control line transparency
+    
+    # line connect data
+    geom_line(aes(group = interaction(plasmid, `AHL (uM)`), 
+                  alpha = `AHL (uM)`)) +  
+    
+    facet_wrap(vars(organism), scale = 'free_y') +
+    theme(legend.position = 'top') +
+    
+    # labels
+    ggtitle(str_c('Memory in wastewater : ', filter_target), subtitle = title_name)
+}
 
 copies_flipped <- plot_timeseries_target() %>% print
 ggsave(plot_as(title_name, '-copy-flip'), copies_flipped, width = 5, height = 5)
@@ -104,3 +120,4 @@ ggsave(plot_as(title_name, '-copy-bb'), width = 5, height = 5)
 
 copies_bb <- plot_timeseries_target('chromosome') %>% print
 ggsave(plot_as(title_name, '-copy-chr'), width = 5, height = 5)
+

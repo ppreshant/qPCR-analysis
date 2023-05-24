cec8764e9d5cfd3db0a840659b70788e3a6de81b
ppreshant <ppresh.ant@gmail.com>
2023-05-24 14:22:47 -0500

    putting controls at the end

--- adhoc scripts/ddPCR_analysis.R
+++ adhoc scripts/ddPCR_analysis.R
@@ -22,8 +22,9 @@ processed_data <- get_data %>%
   left_join(plate_template) %>%  # join template
   rename(Target_name = Target) %>% # rename target to qPCR format
   # separate(Sample, into = c('Sample_category', 'assay_variable'), sep = '_') # new columns for S057j
-
-  mutate(across(Concentration, as.numeric)) # force conc to be numeric
+  mutate(across(Concentration, as.numeric)) %>%  # force conc to be numeric
+  
+  mutate(across(Sample_category, ~ fct_relevel(.x, 'C-', 'ntc', 'control', after = Inf))) # bring controls to the end
 
 ratio_data <- processed_data %>% 
   select(Sample_category, assay_variable, Well, Target_name, Concentration) %>% 
@@ -36,17 +37,30 @@ ratio_data <- processed_data %>%
 # plot ----
 
 # plot copy number
-plot_facetted_assay(.data = ratio_data, 
+copy_num <- 
+  plot_facetted_assay(.data = ratio_data, 
                     .yvar_plot = copy_number, .xvar_plot = assay_variable, 
-                    .facetvar_plot = NULL) + 
+                    .facetvar_plot = NULL) 
+
+copy_num + 
   geom_line(aes(group = Sample_category), alpha = 0.2, show.legend = F) # connect points for easy visual
 
 ggsave(plot_as(title_name, '-ddPCR'), width = 4.2, height = 3)
 
+ggplotly(copy_num)
+
 # plot copies per target
+copies_all_targets <- 
 plot_facetted_assay(.data = filter(processed_data, !str_detect(assay_variable, '1e3')), 
                     .yvar_plot = CopiesPer20uLWell, .xvar_plot = assay_variable, 
                     .facetvar_plot = Target_name) + 
   geom_line(aes(group = Sample_category), alpha = 0.2, show.legend = F) # connect points for easy visual
 
+ggsave(plot_as(title_name, '-ddPCR_raw'), width = 4.2, height = 4)
+
+ggplotly(copies_all_targets)
+
+# custom plotting
+
+copies_all_targets + ylim(c(0, 4300))
 ggsave(plot_as(title_name, '-ddPCR_raw'), width = 4.2, height = 4)

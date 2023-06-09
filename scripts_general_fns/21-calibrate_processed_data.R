ed77c9076a696fdae4461ade9c20af0c0fe35bb9
ppreshant <ppresh.ant@gmail.com>
2023-06-09 16:07:10 -0500

    vectorized for multiple files; using function wrapper - works for S071

--- scripts_general_fns/21-calibrate_processed_data.R
+++ scripts_general_fns/21-calibrate_processed_data.R
@@ -11,50 +11,71 @@ title_name <- base_title_name
 
 axislabel.assay_variable <- 'Template name' # printed on the x axis of the graph
 
-# Load data ----
-cq.dat <- get_processed_datasets(flnm)
 
-# change version of target (-v0 ; for fGFP)
-# cq.dat <- cq.dat %>% mutate(across(Target_name, ~ str_c(.x, '-v0')))
-
-
-# Get std curve ----
-std_to_retrieve <- default_std.to.retrieve
-
-std_par <- 
-  {if(template_source == 'googlesheet')
-    googlesheets4::read_sheet(sheeturls$plate_layouts_PK, sheet = 'qPCR Std curves', range = 'A:G', col_types = 'ccnnnnn') else {
-      read_csv(file = 'qPCR analysis/Standards/qPCR_Std_curve_parameters.csv')}
-  } %>% 
+# Calibration wrapper ----
+#' wrapper function for vectorized calibration
+load_procesessed_data_and_calibrate <- function(flnm, make_plot = FALSE)
+{
+  # Load data ----
+  cq.dat <- get_processed_datasets(flnm)
   
-  filter(str_detect(ID, std_to_retrieve ))
+  # change version of target (-v0 ; for fGFP)
+  # cq.dat <- cq.dat %>% mutate(across(Target_name, ~ str_c(.x, '-v0')))
+  
+  
+  # Get std curve ----
+  if(!exists('std_par')) # ignore this if parameters are already loaded
+  {
+    std_to_retrieve <<- default_std.to.retrieve # load as global variable
+    
+    std_par <<- 
+      {if(template_source == 'googlesheet')
+        googlesheets4::read_sheet(sheeturls$plate_layouts_PK, sheet = 'qPCR Std curves', range = 'A:G', col_types = 'ccnnnnn') else {
+          read_csv(file = 'qPCR analysis/Standards/qPCR_Std_curve_parameters.csv')}
+      } %>% 
+      
+      filter(str_detect(ID, std_to_retrieve ))
+  }
+  
+  
+  
+  # Calibration ----
+  absolute_dat <- calculate_absolute_quant(cq.dat, std_par)
+  
+  
+  # Plot ----
+  if(make_plot)
+  {
+    plt.copies_w.mean <- plot_facetted_assay(.data = absolute_dat, 
+                                             .yvar_plot = Copies.per.ul.template, 
+                                             .xaxis.label.custom = axislabel.assay_variable, 
+                                             points_plt.style = 'jitter', flipped_plot = F) + 
+      geom_boxplot(aes(y = mean_Copies.per.ul.template), show.legend = FALSE)
+    
+    ggsave(plot_as(title_name, '-calibrated'), width = 14, height = 4)
+    
+  }
+  
+  
+  # Save data ----
+  
+  # save backup of uncalibrated data
+  write_csv(cq.dat,
+            str_c('excel files/processed_data/uncalibrated/', flnm, '-processed.csv', sep = ''),
+            na = '')
+  
+  
+  # overwrites existing data
+  write_csv(absolute_dat,
+            str_c('excel files/processed_data/', flnm, '-processed.csv', sep = ''),
+            na = '')
+  
+  return(absolute_dat) # return absolute data from the function
+  
+}
 
+# Run calibration workflow ----
+load_procesessed_data_and_calibrate(flnm, make_plot = T)
 
-# Calibration ----
-absolute_dat <- calculate_absolute_quant(cq.dat, std_par)
-
-
-# Plot ----
-
-plt.copies_w.mean <- plot_facetted_assay(.data = absolute_dat, 
-                                         .yvar_plot = Copies.per.ul.template, 
-                                         .xaxis.label.custom = axislabel.assay_variable, 
-                                         points_plt.style = 'jitter', flipped_plot = F) + 
-  geom_boxplot(aes(y = mean_Copies.per.ul.template), show.legend = FALSE)
-
-ggsave(plot_as(title_name, '-calibrated'), width = 14, height = 4)
-
-
-# Save data ----
-
-# save backup of uncalibrated data
-write_csv(cq.dat,
-          str_c('excel files/processed_data/uncalibrated/', flnm, '-processed.csv', sep = ''),
-          na = '')
-
-
-# overwrites existing data
-write_csv(absolute_dat,
-          str_c('excel files/processed_data/', flnm, '-processed.csv', sep = ''),
-          na = '')
-
+# vectorized call : store all files to calibrate in flnms
+all_absolute_data <- map(flnms, ~ load_procesessed_data_and_calibrate(., make_plot = FALSE))

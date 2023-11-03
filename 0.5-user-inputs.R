fb848937b5e478d4336e19540c1eb0d3105109c1
ppreshant <ppresh.ant@gmail.com>
2023-11-03 17:35:00 -0500

    Moved default standard curve options to the top

--- 0.5-user-inputs.R
+++ 0.5-user-inputs.R
@@ -4,7 +4,11 @@
 
 # File name ----
 
-flnm <- 'q48a_S071_22-4-23' # mention the file name without the "-linreg" or "-processed" suffixes
+flnm <- 'q50_S080_RAM 80a_mjd61 repression_new barcode' # mention the file name without the "-linreg" or "-processed" suffixes
+
+# Default standards : When using same standards for multiple plates (not considered best practice..but I do it anyways)
+default_std.to.retrieve <-  'Std30' # if the file name doesn't hold any std curve, it will default to this (Std30 for RAM)
+skip.std.curves_already.exist <- TRUE # If TRUE, will retrieve std curve data from the google sheet, instead of file. 
 
 # Options ----
 
@@ -16,21 +20,19 @@ template_source <- 'googlesheet' # googlesheet/excel = parse through a list of t
 run_linregpcr <- TRUE
 
 # Traditional Cq analysis - options
-plot_mode <-  'raw_quantification' # Options : ('absolute_quantification' or 'raw_quantification'); 
+plot_mode <-  'absolute_quantification' # Options : ('absolute_quantification' or 'raw_quantification'); 
 # absolute_quantification = Will calculate copy #'s based on y_intercept and slope from standard curve.. 
 # ..calculated or gathered from old std curves 
 # raw_quantification = Cq values are plotted
 
 # Standard curve options
-skip.std.curves_already.exist <- TRUE # If TRUE, will retrieve std curve data from the google sheet
 # This will pick the standard curve within the filename '.._Stdx_..', or use default if not found
-# This pro-active user setting prevents duplicates being processed into the sheet when rerunning script
-# FALSE implies stds will be processed from file
-dilutions_to_truncate <- 0 # indicate the last n dilutions to be trimmed away before making std curve, default 0
+# This proactive user setting prevents duplicates being processed into the sheet when rerunning script
+# FALSE implies stds will be processed from file - if they exist
+dilutions_to_truncate <- 3 # indicate the last n dilutions to be trimmed away before making std curve, default 0
 
-# Default standards : When using same standards for multiple plates (not considered best practice..)
-default_std.to.retrieve <-  'Std30' # if the file name doesn't hold any std curve, it will default to this
-force.use_default.std <- TRUE # forces the use of default std from google sheet, instead of from current file
+# Options for default standards
+force.use_default.std <- TRUE # forces the use of default std from google sheet, if skip.std.curves_already.exist is TRUE
 
 
 # error check ----

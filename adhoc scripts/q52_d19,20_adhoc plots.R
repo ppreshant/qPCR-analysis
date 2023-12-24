40d0b3aa5f7f790bb5e2fa29bd96d6b3bc18688a
ppreshant <ppresh.ant@gmail.com>
2023-12-24 03:38:58 -0600

    Making code self-sufficient with prerequisites and data input

--- adhoc scripts/q52_d19,20_adhoc plots.R
+++ adhoc scripts/q52_d19,20_adhoc plots.R
@@ -1,5 +1,18 @@
 # q52_d19,20_adhoc plots.R
 
+# Prerequisites ----
+source('./0-general_functions_main.R') # Source the general_functions file before running this
+
+# Source user inputs  ----
+# choose file name, title for plots (file name starts in the same directory as Rproject)
+
+source('./0.5-user-inputs.R') # source the user inputs from a different script
+title_name <- base_title_name
+
+# Load data ----
+absolute_dat <- get_processed_datasets(flnm)
+
+# Subset data ----
 
 # D19 data : 1280 series + ntc + MG1655
 d19 <- absolute_dat %>% 
@@ -41,3 +54,10 @@ plt_d19 <- {plot_facetted_assay(.data = d19,
 
 
 ggsave(plot_as('q52_D19_zoom'), plt_d19, width = 5, height = 5)
+
+
+# Target_ratio ----
+source('scripts_general_fns/23-RAM_ratios_wrappers.R')
+
+# work in progress ~ 
+target_ratio <- calc_ram_ratios() %>% view # summarize not working...

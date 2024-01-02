486e9ca76d783eb5b9a6dbc8a3ab0810906f2e8b
ppreshant <ppresh.ant@gmail.com>
2024-01-01 21:10:48 -0600

    removed `summary` features, returns simple tibble instead of list - doing summary in the calling function through `mutate` now

--- scripts_general_fns/23-RAM_ratios_wrappers.R
+++ scripts_general_fns/23-RAM_ratios_wrappers.R
@@ -1,9 +1,9 @@
 # 23-RAM_ratios_wrappers.R
 
-#' wrapper function to make ratios of U64/16S, gfpbarcode/16S and U64/gfpbarcode per biological replicate 
+#' wrapper function to calculate ratios of U64/16S, gfpbarcode/16S and U64/gfpbarcode per biological replicate 
 #' NOTE: all replicates are assumed biological ; need to think how to incorporate technical which I don't do..
 #' @param .df data frame holding the absolute copy number data (or est. copy). Defaults to `absolute_dat`
-#' @returns pivot data with ratios designated as U64_per_16S, gfpbarcode_per_16S, U64_per_gfpbarcode
+#' @returns pivot data with ratios designated as U64_per_16S, gfpbarcode_per_16S, U64_per_gfpbarcode added within the Target_name column
 
 calc_ram_ratios <- function(.df = absolute_dat)
 {
@@ -23,28 +23,38 @@ calc_ram_ratios <- function(.df = absolute_dat)
            gfpbarcode_per_16S = gfpbarcode/`16s`,
            U64_per_gfpbarcode = U64/gfpbarcode
            ) %>% 
+  
+    # bring all targets and ratios back into the Target_name column
+    pivot_longer(cols = matches('U64|gfpbarcode|16s'), # select all targets and ratios ('_per_' for only ratios)
+                 names_to = 'Target_name',
+                 values_to = copies_colname) %>% 
     
-    # summarize (for future summarizing)
-    summarize(across(c(U64, gfpbarcode, `16s`, # choose each raw column
-                       U64_per_16S, gfpbarcode_per_16S, U64_per_gfpbarcode), # and each ratio
-                     
-                     mean), # and get the mean across biological replicates
-              .by = c(Sample_category, matches('assay_var'))) # for each category and sample
+    # take average of biological replicates
+    mutate(across(any_of(copies_colname),
+           # c(U64, gfpbarcode, `16s`, # choose each raw column
+           #      U64_per_16S, gfpbarcode_per_16S, U64_per_gfpbarcode), # and each ratio
+           
+           lst(mean), # and get the mean across biological replicates
+           .names = '{.fn}_{.col}'), # name columns as mean_..
+  
+           .by = c(Sample_category, Target_name, matches('assay_var')))  # for each category and sample
+  
+
   
 }
 
-#' wrapper function to make ratios of Maximal/Repressed per target or signal variable, per biological replicate
+#' wrapper function to calculate ratios of Maximal/Repressed per target or signal variable, per biological replicate
 #' @param .df data frame holding the absolute copy number data (or est. copy). Defaults to `absolute_dat`
 #' @returns pivot data with ratios designated as repression_ratio
 
-calc_repression_ratios <- function(.df)
+calc_repression_ratios <- function(.df = target_ratio)
 {
   fold_repression <- 
     .df %>% 
     
     # cleanup samples that are not paired for repressed and maximal
     filter(!str_detect(assay_var.label, 'ntc'), # remove ntc
-           !str_detect(Target_name, '16s')) %>% # remove 16S -- not expected biologically to see variation
+           !str_detect(Target_name, '^16s')) %>% # remove 16S -- not expected biologically to see variation
     
     # select relevant columns and pivot to make Maximal and Repressed into separate columns
     select(Target_name, Sample_category, assay_var.label, mean_Copies.per.ul.template) %>% 
@@ -54,23 +64,5 @@ calc_repression_ratios <- function(.df)
     mutate(repression_fold = Maximal/Repressed, # calculate ratio of the averages of Max / Repressed
            Sample_category = 'all') # add column for plotting
   # Note: doing ratio of averages since the replicates are not paired
-  
-  # show mean data for quick reference
-  summarise(fold_repression, 
-            avg_fold_repression = repression_fold, # take average
-            .by = c(Target_name, assay_var.label)) %>%  # for each target and design
-    pivot_wider(names_from = Target_name, values_from = avg_fold_repression)
-  
-  
-  # plot fold change due to repression ----
-  plt.fold_repression <-
-    plot_facetted_assay(.data = fold_repression, 
-                        .yvar_plot = repression_fold) + 
-    
-    aes(label = biological_replicates) # add label for interactive plot
-  
-  # interactive plot
-  ggplotly(plt.fold_repression)
-  
-  
+
 }

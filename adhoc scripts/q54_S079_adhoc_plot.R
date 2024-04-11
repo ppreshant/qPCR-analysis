607ba9a1c0c4dc34365cd715a014baf579a0d861
ppreshant <ppresh.ant@gmail.com>
2024-04-10 20:06:15 -0500

    plotting ratio to 16S - doesn't change order but shows that expression is similar in Ab, So, Vn

--- adhoc scripts/q54_S079_adhoc_plot.R
+++ adhoc scripts/q54_S079_adhoc_plot.R
@@ -39,6 +39,34 @@ abs2 <-
                   fct_relevel(plasmid_translation) # order them as above!
                 ) ) 
 
+
+## Ratios to 16S ----
+
+# Modified code from 2H_all_organisms.R script of RAM
+# Calculating spliced fraction of the Ribozyme/16s
+ratio_wider <- 
+  
+  abs2 %>% 
+  
+  # select only desired columns (for pivoting)
+  select(Target_name,  assay_variable, assay_var.horz_label, Copies.per.ul.template, biological_replicates) %>%  # select only required columns
+  rename(organism = assay_var.horz_label) %>%  # rename column to organism
+  
+  pivot_wider(names_from = Target_name, values_from = Copies.per.ul.template, names_prefix = 'copies_') %>%  # each target to a col
+  
+  # calculate ratios of each targets per replicate
+  mutate(spliced_per_ribozyme = copies_U64 / copies_gfpbarcode,
+         spliced_per_16S = copies_U64 / `copies_16s`, 
+         expresssion_per_16S = copies_gfpbarcode / `copies_16s`) %>% 
+  
+  # find the mean across biological replicates for all numeric cols
+  group_by(assay_variable) %>% 
+  mutate(across(where(is.numeric), 
+                \ (x) mean(x, ignore.na = TRUE),
+                .names = "mean_{.col}")) 
+
+
+
 # plotting ---- 
 
 
@@ -62,25 +90,14 @@ ggsave(plot_as('q54_S079_RNA2'), width = 7, height = 5)
 
 ## Expression vs splicing ----
 
-copies_wider <-
-  
-  abs2 %>% 
-  
-  # select only desired columns
-  select(Target_name, assay_var.horz_label, Copies.per.ul.template, biological_replicates) %>%  # select only required columns
-  rename(organism = assay_var.horz_label) %>%  # rename column to organism
-  
-  pivot_wider(names_from = Target_name, values_from = Copies.per.ul.template, names_prefix = 'Copies_') # each target to a col
-  
-
 
 plt.positivesonly_correlation <- 
   {ggplot(
     
     # remove unnecessary data for this plot
-    filter(copies_wider, !str_detect(organism, 'NTC|(-)|552')), 
+    filter(ratio_wider, !str_detect(organism, 'NTC|(-)|552')), 
     
-    aes(x = `Copies_gfpbarcode`, y = `Copies_U64`,
+    aes(x = `copies_gfpbarcode`, y = `copies_U64`,
         label = organism)) + 
       
       geom_point() + 
@@ -102,3 +119,36 @@ plt.positivesonly_correlation <-
 # save plot
 ggsave(plot_as(title_name, '_expr vs splice'), width = 3, height = 4)
 
+
+### Ratio to 16S ----
+
+plt_correlation_normalize16s <- 
+  {ggplot(
+    
+    # remove unnecessary data for this plot
+    filter(ratio_wider, !str_detect(organism, 'NTC|(-)|552')), 
+    
+    aes(x = expresssion_per_16S, y = `spliced_per_16S`,
+        label = organism)) + 
+      
+      geom_point() + 
+      
+      # add labels on the points with organism name
+      ggrepel::geom_text_repel(nudge_y = 1) + 
+      
+      # ggforce::geom_mark_rect(aes(label = organism)) + labelling points does not work
+      geom_smooth(method = 'lm', show.legend = FALSE, alpha = 0.2) + # draw a correlation line
+      
+      
+      labs(title = title_name)
+    
+  } %>%
+  
+  format_logscale_x() %>%
+  format_logscale_y()
+
+
+ggplotly(plt_correlation_normalize16s, dynamicTicks = T)
+
+# save plot
+ggsave(plot_as(title_name, '_expr vs splice_per16S'), width = 3, height = 4)

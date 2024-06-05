9de9ec29db43fc166604eed7c25f39888c7b4e1c
ppreshant <ppresh.ant@gmail.com>
2024-06-04 22:07:21 -0500

    remove U64, direct label, size ~ S10B

--- adhoc scripts/RAM/S10c_q25.R
+++ adhoc scripts/RAM/S10c_q25.R
@@ -12,23 +12,12 @@ source('./0-general_functions_main.R') # Source the general_functions file befor
 flnm <- 'q25_S037_RAM repression_14-2-22' # overwrites 
 title_name <- 'q25_S10c_RAM repression' # make title_name for plot and saving
 
-absolute_dat <- get_processed_datasets(flnm) %>% 
+absolute_dat <- get_processed_datasets(flnm)
   
   
 
 # name translators ----
 # COPIED FROM ramfacs_2b_repression fusions.R
-
-assay_var_translation <- c('76' = 'Ribozyme',    # 'current name' = 'new name' format
-                           # '48' = 'mScarlet',  # regex based translation to change x-axis labels
-                           
-                           '51' = 'Ribozyme-mScarlet',
-                           '77' = 'mScarlet-Ribozyme v2',
-                           '79' = 'mScarlet-Ribozyme',
-                           
-                           '67 \\+ ' = '', # remove extra information
-                           ' \\+ 67' = '', # the special symbol '+' needs to be double escaped
-                           'NTC' = 'ntc')
 
 sample_category_cleaner <- c('test' = 'Repressed', # clean up redundant names
                              'repressed' = 'Repressed',
@@ -37,17 +26,18 @@ sample_category_cleaner <- c('test' = 'Repressed', # clean up redundant names
 
 
 # used only for presentation stuff (extra analysis)
-target_translator <- c('16s' = '16S',
-                       'gfpbarcode' = 'Ribozyme expression',
-                       'U64' = 'Spliced product')
+target_translator <- c('16s' = '16S rRNA',
+                       'gfpbarcode' = 'cat-RNA',
+                       'U64' = 'barcoded rRNA')
 
 # Processing data ------------------------------
 
 # create a subset of data for presenting : ONLY 76
 processed_data <- filter(absolute_dat,
-                           str_detect(assay_variable, '76')) %>% # Select only 76
+                           str_detect(assay_variable, '76'), # Select only 76
+                         !str_detect(Target_name, 'U64')) %>%  # remove U64
   
-  mutate(across(assay_variable, ~ str_replace_all(.x, assay_var_translation))) %>%  # assign labels for readability
+  mutate(assay_variable = 'Ribozyme') %>%  # assign labels for readability
   mutate(across(Sample_category, ~ str_replace_all(.x, sample_category_cleaner))) %>%  # clean up redundant names
   
   # rename target for ease
@@ -56,10 +46,10 @@ processed_data <- filter(absolute_dat,
 
 label_data <- 
   select(processed_data, Target_name, Sample_category, mean_Copies.per.ul.template) %>%
-  unique() # select unique values
-  # 
-  # # round off mean values
-  # mutate(mean_Copies.per.ul.template = round(mean_Copies.per.ul.template, 1))
+  unique() %>%  # select unique values
+  
+  # add a column for labels to cat-RNA (3,4)
+  mutate(label_categories = c(NA, NA, 'Repressed', 'Constitutive') )
   
 
 # plot ------------------------------
@@ -68,27 +58,36 @@ label_data <-
         aes(y = Target_name, x = Copies.per.ul.template, 
             shape = Sample_category)) + 
     
-    geom_jitter(width = 0.2, alpha = 0.5) + 
+    geom_jitter(width = 0, height = 0.2, alpha = 0.5) + 
     
     # change shapes
     scale_shape_manual('Expression', values = c('Repressed' = 1, 'Maximal' = 19), ) +
-    
+    # remove legend manually?
     
     # Indicate the mean values
     geom_point(aes(x = mean_Copies.per.ul.template), 
-               shape = '|', size = 3) + 
+               shape = '|', size = 5) + 
     
     # label values
     geom_text(aes(x = mean_Copies.per.ul.template,
                   label = scales::label_scientific(digits = 1)(mean_Copies.per.ul.template),
                   # label = scales::label_parse(mean_Copies.per.ul.template), # for power notation
                   
-                  vjust = -2),  # adjust position of label
+                  vjust = 2),  # adjust position of label
               
               data = label_data) +
 
+    # direct label instead of legend
+    geom_text(aes(x = mean_Copies.per.ul.template,
+                  label = label_categories,
+                  vjust = -2),  # adjust position of label
+              
+              data = label_data) +
     
-    theme_minimal() + theme(legend.position = 'top') +
+    # theme and remove the legend
+    theme_classic() + 
+    
+    theme(legend.position = 'none') +
     
     # adjust labels
     labs(title = NULL, x = 'Copies per ul template', y = NULL)
@@ -103,4 +102,4 @@ label_data <-
 ggsave(plot_as(title_name), width = 6, height = 3) # save plot as png  
 
 ggsave(filename = str_c('qPCR analysis/', title_name, '.pdf'), 
-       dpi = 300, width = 6, height = 3)
+       dpi = 300, width = 4, height = 2.5)

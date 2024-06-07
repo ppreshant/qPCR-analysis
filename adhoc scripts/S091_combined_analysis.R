b6b7dc769460c768fe941eaadbfc3c2acda3ceb6
ppreshant <ppresh.ant@gmail.com>
2024-06-07 07:55:35 -0500

    ordering assay vars in plot. Fix bug ~ replace NAs in flag_wells

--- adhoc scripts/S091_combined_analysis.R
+++ adhoc scripts/S091_combined_analysis.R
@@ -13,18 +13,32 @@ flnms <- c('S091-marine-withsort-5-5-24',
 
 title_name <- 'S091-marine-ddPCR'
 
+## ordering -----
+assay_var_order <- c('J23100', 
+                     'high', 'med', 'low',
+                     'high-growth', 'med-growth', 'low-growth',
+                     'WT', 
+                     'blank', 'NTC') # order the assay variables
+
 
 # load data ----
 
 combined_data <- get_processed_datasets(flnms) %>% 
   relocate(run_ID) %>% # bring run_ID to the front
   
+  # fix NAs in flag_wells
+  replace_na(list(flag_wells = '')) %>% 
+  
   # get 'b' in S0xxb ; else below will be 'a'
   mutate(batch = if_else(str_detect(run_ID, 'b'), 'b', 'a'),
-         .after = run_ID)
+         .after = run_ID) %>% 
+  
+  ## custom processing ordering data ----
+  mutate(across(assay_variable, ~ fct_relevel(.x, assay_var_order))) # order the assay variables
 
 
 # Processing ----
+
 
 # (pcn): Ratio: plasmid/chromosome Swetha's marine ddPCR data
 ratio_data <- combined_data %>% 
@@ -42,12 +56,50 @@ ratio_data <- combined_data %>%
   mutate(copy_number = Plasmid / Chromosome)
 
 
-# adhoc ----
-filter(combined_data,
-       flag_wells == 'Manual') %>%
-       # (flag_wells == 'Below LOD' &  Target_name == 'Chromosome')) %>% 
-  relocate(PositiveDroplets, LOD_max_pos_droplets) %>%
-  view
+# remove flagged wells (manual)
+plotting_data <- combined_data %>% 
+  filter(!flag_wells == 'Manual') # remove manually flagged wells
+
+
+## get LOD ----
+
+LOD <- select(combined_data, 
+              Target_name, run_ID,
+              LOD_mean_copieswell, LOD_max_copieswell, LOD_max_pos_droplets) %>% 
+  unique() %>%
+  
+  # select the maximum ones between different runs
+  reframe(across(matches('LOD'), max), .by = Target_name)
+
+
+## adhoc/checking ----
+if(0)
+{
+  
+  # view stuff below LOD
+  filter(combined_data,
+         # flag_wells == 'Manual') %>%
+    (flag_wells == 'Below LOD' &  Target_name == 'Chromosome'),
+     Sample_category != 'Negative') %>%
+    
+    relocate(PositiveDroplets, LOD_max_pos_droplets) %>%
+    view
+  
+  # filter view
+  filter(combined_data, flag_wells == 'Manual') %>% view
+  
+  # (obsolete) filter with NAs 
+  # Source: https://stackoverflow.com/a/74756923/9049673
+  filter_na <- function(tbl, expr){
+    tbl %>% filter({{expr}} %>% replace_na(T))
+  }
+  
+  # check vals == LOD
+  filter(combined_data, PositiveDroplets == LOD_max_pos_droplets) %>% 
+    relocate(PositiveDroplets, LOD_max_pos_droplets, Target_name, .after = 4) %>%
+    view
+  
+}
 
 
 # Plotting ----
@@ -56,8 +108,8 @@ filter(combined_data,
 
 # plot copies per target
 copies_all_targets <- 
-  plot_facetted_assay(.data = 
-                        filter(combined_data, !flag_wells == 'Manual'), # removed flagged wells
+  {plot_facetted_assay(.data = 
+                         filter(combined_data, !flag_wells == 'Manual'), # removed flagged wells
                       
                       .yvar_plot = CopiesPer20uLWell, .xvar_plot = assay_variable, 
                       .label.var = Well,
@@ -65,7 +117,9 @@ copies_all_targets <-
   
   # add LOD line
   geom_hline(data = LOD, aes(yintercept = LOD_max_copieswell), linetype = 'dashed', color = 'red') +
-  geom_hline(data = LOD, aes(yintercept = LOD_mean_copieswell), linetype = 'dashed', color = 'gray')
+  geom_hline(data = LOD, aes(yintercept = LOD_mean_copieswell), linetype = 'dashed', color = 'gray')} %>% 
+  
+  print
 
 # geom_line(aes(group = Sample_category), alpha = 0.2, show.legend = F) # connect points for easy visual
 
@@ -97,6 +151,25 @@ ggplotly(copy_num)
 
 
 
+## separate panels ----
+
+### sort original -----
+
+sorted_data <- filter(ratio_data, str_detect(assay_variable, 'low$|med$|high$'))
+
+# plot copies per target faceted by organism
+
+copy_number_sorted <- 
+  plot_facetted_assay(.data = sorted_data, 
+                      .yvar_plot = copy_number, .xvar_plot = assay_variable,
+                      .colourvar_plot = NULL,
+                      .label.var = Well, 
+                      flipped_plot = FALSE, facet_scale_constraint = 'free',
+                      .facetvar_plot = Sample_category) + 
+  facet_wrap(facets = vars(Sample_category), scales = 'free')
+
+
+ggplotly(copy_number_sorted) # interactive plot
 
 
 # adhoc ----
